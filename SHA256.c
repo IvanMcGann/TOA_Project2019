c2606b3bf0ba7eed8a38290fae3e4c708ad76a31
IvanMcGann <imcgann1989@theory-of-algorithms-19.us-east1-b.c.gentle-epoch-230115.internal>
2019-03-11 15:49:10 +0000

    added hash computation functions, maj and ch not implemented yet

--- SHA256.c
+++ SHA256.c
@@ -28,11 +28,11 @@ int main(int argc, char *argv[]){
 
 void sha256(){
 
-   // Message schedule: (Section 6.2)	
+   // Message schedule (Section 6.2)	
    uint32_t W[64];
-   // Working variables: (Section 6.2)
+   // Working variables (Section 6.2)
    uint32_t a, b, c, d, e, f, g, h;
-   //Temporary Variables: (Section 6.2) 
+   //Temporary Variables (Section 6.2) 
    uint32_t T1, T2;
    //hash value: literal numbers wriiten in hexidecimal (Section 5.3.3)	    
    uint32_t H[8] = {
@@ -46,12 +46,17 @@ void sha256(){
     0x5be0cd19	  
     };
 
-   // Current message block: (Section 6.2.2)
+    
+  // Current message block (Section 6.2.2)
    uint32_t M[16];
    
-   //Used for looping
+   //Used for looping below
    int t;
-	
+   
+   //SHA-256 hash computation uses functions and constants previously defined in( Sections 4.1.2 & 4.2.2)
+   //(Section 6.2.2)
+   
+   //Part 1. (Section 6.2.2)
    //W[t] = M[t]...for 0 <= t <= 15 (Section 6.2.2)	
    for (t = 0; t < 16; t++)
     W[t] = M[t];
@@ -59,11 +64,42 @@ void sha256(){
    //W[t] = Sigma1 for 16 <= t <= 63	
    for (t = 16; t < 64; t++) 
     W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
-   //
-   //
+   
+   //Part 2. (Section 6.2.2)
+   //Initialize the eight working variables, a, b, c, d, e, f, g, and h, with the (i-1)st hash value
+   a = H[0]; b = H[1]; c = H[2]; d = H[3];
+   e = H[4]; f = H[5]; g = H[6]; h = H[7];
 
+   //Part 3.
+   //For t=0 to 63 (Section 6.2.2)  
+   for (t = 0; t < 64; t++){
+   T1 = h + SIG_1(e) + Ch(e, f, g) + K[t] + W[t];
+   T2 = SIG_0(a) + Maj(a, b, c);
+   h = g;
+   g = f;
+   f = e;
+   e = d + T1;
+   d =c;
+   c = b;
+   b = a;	
+   a = T1 + T2;
+   }
+  //Part 4.
+  //Compute the ith intermediate hash value H(i) (Section 6.2.2)  
+  
+   H[0] = a + H[0];  
+   H[1] = b + H[1];
+   H[2] = c + H[2];
+   H[3] = d + H[3];
+   H[4] = e + H[4];
+   H[5] = f + H[5];
+   H[6] = g + H[6];
+   H[7] = h + H[7];
 }
-   // x is a w-bit word and n is an integer with 0 <= n <= w (Section 3.2)
+    //^ = XOR , | = OR
+   
+
+   //x is a w-bit word and n is an integer with 0 <= n <= w (Section 3.2)
    uint32_t rotr(uint32_t n, uint32_t x){
     return (x >> n) | (x << (32 - n));
    }
@@ -72,7 +108,9 @@ void sha256(){
     return (x >> n);
    }
 
-   //^ = XOR , | = OR 
+   //^ = XOR , | = OR
+   
+
    //(Sections 3.2 & 4.1.2)
    uint32_t sig0(uint32_t x){
     return (rotr(7, x) ^ rotr(18, x) ^ shr(3, x));
