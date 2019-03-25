21503c0e00d71ba2cdb9a3513b4580c1ab8b55d6
IvanMcGann <imcgann1989@theory-of-algorithms-19.us-east1-b.c.gentle-epoch-230115.internal>
2019-03-25 15:16:44 +0000

    created union to read message blocks in a variety of ways from same location

--- padfile.c
+++ padfile.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
+//Unions are similar to structs but allow elements to be stored at the same memory location
+union msgblock{
+  uint8_t e[64]; //64 element array of 8 bit integers = 512 byte message block
+  uint32_t t[16]; //16 element array of 32 bit integers = 512 byte message block
+  uint64_t s[8]; //8 element array of 64 bit integers = 512 bit message block
+};
+
+
 int main(int argc, char *argv[]) {
   
   //f is name of the file
@@ -9,11 +17,11 @@ int main(int argc, char *argv[]) {
   //c character is used to read file
   char c;
   
-  //file open command for the first arguement provided on the command line
+  //file open command for the first arguement provided on the command line. Argv allows you do deal with cmd line arguments
   f = fopen(argv[1], "r"); 
-  //stdarg.h helps cmd line arguements for later
+  //stdarg.h helps cmd line arguments for later
  
-  //prints out the first, single character (c) read from pointer in from the file f
+  //prints out the first, single character (c) read from pointer in from the file f. fread deals in bytes, One byte - One time
   printf("%c\n", fread(&c, 1, 1, f);
   
   //closes f pointed at 		  
