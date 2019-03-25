79fec432b60dfaed5b3693afdfe8d57186a02002
IvanMcGann <imcgann1989@theory-of-algorithms-19.us-east1-b.c.gentle-epoch-230115.internal>
2019-03-25 14:50:06 +0000

    test script to read a character from a file, complies and runs

--- padfile.c
+++ padfile.c
@@ -2,7 +2,24 @@
 #include <stdint.h>
 
 int main(int argc, char *argv[]) {
+  
+  //f is name of the file
+  FILE* f;
+  
+  //c character is used to read file
+  char c;
+  
+  //file open command for the first arguement provided on the command line
+  f = fopen(argv[1], "r"); 
+  //stdarg.h helps cmd line arguements for later
+ 
+  //prints out the first, single character (c) read from pointer in from the file f
+  printf("%c\n", fread(&c, 1, 1, f);
+  
+  //closes f pointed at 		  
+  fclose(f); 
 
+  //return statement 	 
 
- return 0;
+  return 0;
 }
