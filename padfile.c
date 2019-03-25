e69f327978dacf5301dbcf0a0e58057a2c36bc10
IvanMcGann <imcgann1989@theory-of-algorithms-19.us-east1-b.c.gentle-epoch-230115.internal>
2019-03-25 23:28:21 +0000

    tidied up layout of padding.c, finished second video on padding

--- padfile.c
+++ padfile.c
@@ -1,61 +1,60 @@
 #include <stdio.h>
 #include <stdint.h>
 
-//Unions are similar to structs but allow elements to be stored at the same memory location
-union msgblock{
-  uint8_t e[64]; //64 element array of 8 bit integers = 512 byte message block
-  uint32_t t[16]; //16 element array of 32 bit integers = 512 byte message block
-  uint64_t s[8]; //8 element array of 64 bit integers = 512 bit message block
-};
+// Unions are similar to structs but allow elements to be stored at the same memory location
+ union msgblock{
+   uint8_t e[64]; //64 element array of 8 bit integers = 512 byte message block
+   uint32_t t[16]; //16 element array of 32 bit integers = 512 byte message block
+   uint64_t s[8]; //8 element array of 64 bit integers = 512 bit message block
+ };
 
 
-int main(int argc, char *argv[]) {
+  int main(int argc, char *argv[]) {
   
-  //message block instance defined above defined union
-  union msgblock M;
+   // message block instance defined above defined union
+   union msgblock M;
 
-  //tracks the amount of bits append to end of message block. 
-  uint64_t nobits = 0;
+   // tracks the amount of bits append to end of message block. 
+   uint64_t nobits = 0;
   
-  //current no of bytes read
-  uint64_t nobytes; //64 bit integer used below
+   // current no of bytes read
+   uint64_t nobytes; //64 bit integer used below
   
-  //f is name of the file pointer
-  FILE* f;
+   // f is name of the file pointer
+   FILE* f;
   
-   
-  //file open command for the first arguement provided on the command line. Argv allows you do deal with cmd line arguments
-  f = fopen(argv[1], "r"); 
-  //stdarg.h helps cmd line arguments for later
+   // file open command for the first arguement provided on the command line. Argv allows you do deal with cmd line arguments
+   f = fopen(argv[1], "r"); 
+   // stdarg.h helps cmd line arguments for later
  
-  //not at end of the file f
-  while(!feof(f)){//fread deals in 64 bytes from f; reads up to not more than; M.e stores the 64 read bytes in a message block
-    nobytes = fread(M.e, 1, 64, f);
-    //64 - 8 check if read block is less than 55 bytes. If it is it enters this statement
-    if (nobytes < 56){
-      printf("I've found a block with less than 55 bytes!\n"); // prints statement to warn if it is
-      //0x80 is seven zeros followed by a one on the left. Put in at the end. 
-      M.e[nobytes] = 0x80;   
-      while (nobytes < 56){
-        nobytes = nobytes + 1;// add one to no bytes
-        M.e[nobytes] = 0x00; //set all bytes to zero, between end of message up to last 8 bytes that need to be appended
-      }
+   // not at end of the file f
+   while(!feof(f)){ // fread deals in 64 bytes from f; reads up to not more than; M.e stores the 64 read bytes in a message block
+     nobytes = fread(M.e, 1, 64, f);
+   // 64 - 8 check if read block is less than 55 bytes. If it is it enters this statement
+     if (nobytes < 56){
+       printf("I've found a block with less than 55 bytes!\n"); // prints statement to warn if it is
+       // 0x80 is seven zeros followed by a one on the left. Put in at the end. 
+       M.e[nobytes] = 0x80;   
+       while (nobytes < 56){
+         nobytes = nobytes + 1;// add one to no bytes
+         M.e[nobytes] = 0x00; // set all bytes to zero, between end of message up to last 8 bytes that need to be appended
+       }
       // message block is array of 8, 64 bit integers, last bit element as nobits
-      // works in modern c standards
-	M.s[7] = nobits;
+      // works in modern c standards to read from same union
+   	 M.s[7] = nobits;
 
-    } 
-  }
+     } 
+   }
     
-  //closes f 		  
-  fclose(f); 
+   // closes file f 		  
+   fclose(f); 
 
-  //this displays all elements of M as 64 bytes in hex
-  for (int i = 0; i < 64; i++)
-  printf("%x ", M.e[i]);
-  printf("\n");
+   // this displays all elements of M as 64 bytes in hex
+   for (int i = 0; i < 64; i++)
+   printf("%x ", M.e[i]);
+   printf("\n");
 
  
-  //return statement 	 
-  return 0;
+   // return statement 	 
+   return 0;
 }
